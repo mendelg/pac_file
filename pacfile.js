4967b73607c1b78ae50bdf607dfb39e7f82b05d8
mendelg <68921220+mendelg@users.noreply.github.com>
2022-11-15 16:08:13 -0600

    Update pacfile.js

--- pacfile.js
+++ pacfile.js
@@ -2,7 +2,7 @@ var normal = "DIRECT", blackhole = "PROXY 127.0.0.1:3421";
 
 function FindProxyForURL(url, host) {
     
-    if(/.*/i.test(url)) {
+    if(/google.com.*/i.test(url)) {
         return blackhole;
     }
     return normal;
